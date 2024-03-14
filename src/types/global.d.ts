0d7384c99997416d61f6e9f3da412829b007d9e6
Emil Öhlund <emil@emilohlund.dev>
2024-03-14 15:56:26 +0100

    fix: :bug: Fixed typings

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,9 +1,12 @@
+import { Client } from "pg";
 import { Datamint } from "../utils/Datamint";
+import { Connection } from "mysql2/promise";
+import { MongoClient } from "mongodb";
 
 declare global {
   namespace globalThis {
-    var __POSTGRESQL_DATAMINT__: Datamint | undefined;
-    var __MYSQL_DATAMINT__: Datamint | undefined;
-    var __MONGODB_DATAMINT__: Datamint | undefined;
+    var __POSTGRESQL_DATAMINT__: Datamint<Client> | undefined;
+    var __MYSQL_DATAMINT__: Datamint<Connection> | undefined;
+    var __MONGODB_DATAMINT__: Datamint<MongoClient> | undefined;
   }
 }
