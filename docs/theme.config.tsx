d127350fa0dc67d16df9aefc6c9cc57461cf117b
Emil Öhlund <emil@emilohlund.dev>
2024-03-14 21:49:11 +0100

    docs: :memo:

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -7,9 +7,25 @@ const config: DocsThemeConfig = {
   project: {
     link: "https://github.com/emilohlund-git/datamint",
   },
-  docsRepositoryBase: "https://github.com/emilohlund-git/datamint",
+  docsRepositoryBase: "https://github.com/emilohlund-git/datamint/docs",
   footer: {
     text: <Logo />,
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Datamint",
+    };
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Datamint" />
+      <meta property="og:description" content="Simple, Available, Dockerized Database Containers" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </>
+  ),
+  nextThemes: {
+    defaultTheme: "dark"
   },
 };
 
